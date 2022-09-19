f96eba7735caf04cf0f9c9a5c0d14c56c3338e6c
Nathan-Kemal <nathankemal4@gmail.com>
2022-09-19 22:35:49 +0300

    removed middleware

--- middleware.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-import { serialize } from "cookie";
-
-export default function middleware(req) {
-  if (req.nextUrl.pathname.startsWith("/home-page")) {
-    NextResponse.next();
-  }
-}
