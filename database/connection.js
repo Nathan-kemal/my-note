2d012a92e4450c53b4008c6f9504e09846b5cb2f
Nathan-Kemal <nathankemal4@gmail.com>
2022-09-24 19:06:36 +0300

    Fixed bug

--- database/connection.js
+++ database/connection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
-const mongoDB = "mongodb://localhost:27017/todoye";
-// const mongoDB = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@cluster0.k8jutf2.mongodb.net/todoye?retryWrites=true&w=majority`;
+// const mongoDB = "mongodb://localhost:27017/todoye";
+const mongoDB = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@cluster0.k8jutf2.mongodb.net/todoye?retryWrites=true&w=majority`;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
